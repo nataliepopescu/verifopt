f82374d4c7ed116c3ebcdb081fe3e9c39da1c82f
Natalie Popescu <nataliepopescu@gmail.com>
2025-08-11 14:48:45 -0400

    more handwritten-rewrite org

--- simple-interp/examples/handwritten.rs
+++ simple-interp/examples/handwritten.rs
@@ -1,31 +1,44 @@
 use rand::Rng;
 use std::any::Any;
 
-trait Animal {
+const BIRD_TYPE: usize  = 0;
+const CAT_TYPE: usize   = 1;
+const DOG_TYPE: usize   = 2;
+
+// if only testing the unsafe (second) example, can remove the `Any` supertrait here
+trait Animal : Any {
     fn speak(&self);
-    fn type_id(&self) -> usize;
+    fn typeid(&self) -> usize;
     fn as_any(&self) -> &dyn Any;
 }
 
 fn get_animal() -> Box<dyn Animal> {
     let num: u32 = rand::rng().random_range(..2);
     if num == 0 {
-        return Box::new(Cat {});
+        return Box::new(Cat { typeid: CAT_TYPE });
     } else {
-        return Box::new(Dog {});
+        return Box::new(Dog { typeid: DOG_TYPE });
     }
 }
 
-struct Bird {}
-struct Cat {}
-struct Dog {}
+struct Bird {
+    typeid: usize,
+}
+
+struct Cat {
+    typeid: usize,
+}
+
+struct Dog {
+    typeid: usize,
+}
 
 impl Animal for Bird {
     fn speak(&self) {
         println!("chirp");
     }
-    fn type_id(&self) -> usize {
-        0
+    fn typeid(&self) -> usize {
+        self.typeid
     }
     fn as_any(&self) -> &dyn Any {
         self
@@ -36,8 +49,8 @@ impl Animal for Cat {
     fn speak(&self) {
         println!("meow");
     }
-    fn type_id(&self) -> usize {
-        1
+    fn typeid(&self) -> usize {
+        self.typeid
     }
     fn as_any(&self) -> &dyn Any {
         self
@@ -48,8 +61,8 @@ impl Animal for Dog {
     fn speak(&self) {
         println!("woof");
     }
-    fn type_id(&self) -> usize {
-        2
+    fn typeid(&self) -> usize {
+        self.typeid
     }
     fn as_any(&self) -> &dyn Any {
         self
@@ -70,15 +83,15 @@ fn any_safe() {
 fn typeid_transmute_unsafe() {
     let animal = get_animal();
 
-    let ti = animal.type_id();
+    let typeid = animal.typeid();
 
     let rawptr = Box::into_raw(animal) as *const ();
-    if ti == 1 {
+    if typeid == 1 {
         unsafe {
             let cat: &Cat = std::mem::transmute::<*const (), &Cat>(rawptr);
             <Cat as Animal>::speak(cat);
         }
-    } else if ti == 2 {
+    } else if typeid == 2 {
         unsafe {
             let dog: &Dog = std::mem::transmute::<*const (), &Dog>(rawptr);
             <Dog as Animal>::speak(dog);

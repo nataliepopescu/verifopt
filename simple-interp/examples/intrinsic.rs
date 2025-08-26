7df1ef04086e61161d5254c645a55d2cb41ca1e7
Natalie Popescu <nataliepopescu@gmail.com>
2025-08-26 14:55:32 -0400

    any's typeid?

--- simple-interp/examples/intrinsic.rs
+++ simple-interp/examples/intrinsic.rs
@@ -1,14 +1,16 @@
-#![feature(core_intrinsics)]
+//#![feature(core_intrinsics)]
 
-trait Animal {
+use std::any::{Any, TypeId};
+
+trait Animal: Any {
     fn speak(&self);
 }
 
-fn get_animal(num: usize) -> (Box<dyn Animal>, u128) {
+fn get_animal(num: usize) -> Box<dyn Animal> { //, u128) {
     if num == 0 {
-        return (Box::new(Cat {}), core::intrinsics::type_id::<Cat>());
+        return Box::new(Cat {}); //, core::intrinsics::type_id::<Cat>());
     } else {
-        return (Box::new(Dog {}), core::intrinsics::type_id::<Dog>());
+        return Box::new(Dog {}); //, core::intrinsics::type_id::<Dog>());
     }
 }
 
@@ -44,15 +46,19 @@ fn main() {
         1 => println!("Pass in a number and see what happens!"),
         _ => {
             let num = args[1].parse().unwrap();
-            let (animal, typeid) = get_animal(num);
+            let animal = get_animal(num);
+            let typeid = animal.type_id();
             let rawptr = Box::into_raw(animal) as *const ();
+            println!("dyn animal typeid: {:?}", typeid);
+            println!("cat typeid: {:?}", TypeId::of::<Cat>());
+            println!("dog typeid: {:?}", TypeId::of::<Dog>());
 
-            if typeid == core::intrinsics::type_id::<Cat>() {
+            if typeid == TypeId::of::<Cat>() { //core::intrinsics::type_id::<Cat>() {
                 unsafe {
                     let cat: &Cat = std::mem::transmute::<*const (), &Cat>(rawptr);
                     <Cat as Animal>::speak(cat);
                 }
-            } else if typeid == core::intrinsics::type_id::<Dog>() {
+            } else if typeid == TypeId::of::<Dog>() { //core::intrinsics::type_id::<Dog>() {
                 unsafe {
                     let dog: &Dog = std::mem::transmute::<*const (), &Dog>(rawptr);
                     <Dog as Animal>::speak(dog);

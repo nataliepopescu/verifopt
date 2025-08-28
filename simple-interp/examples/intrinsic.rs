4c52cec186a8ec418b2b009a84ec5cedabdf0e5c
Natalie Popescu <nataliepopescu@gmail.com>
2025-08-28 13:39:36 -0400

    rename handwritten example and try some intrinsics

--- simple-interp/examples/intrinsic.rs
+++ simple-interp/examples/intrinsic.rs
@@ -1,4 +1,4 @@
-//#![feature(core_intrinsics)]
+#![feature(core_intrinsics)]
 
 use std::any::{Any, TypeId};
 
@@ -47,11 +47,34 @@ fn main() {
         _ => {
             let num = args[1].parse().unwrap();
             let animal = get_animal(num);
+            let cat = Cat{};
+            let dog = Dog{};
+            unsafe {
+                let (animal_pointer, animal_vtable) = std::mem::transmute_copy::<Box<dyn Animal>, (*const u8, *const usize)>(&animal);
+                let (cat_pointer, cat_vtable) = std::mem::transmute_copy::<Cat, (*const u8, *const usize)>(&cat);
+                let (dog_pointer, dog_vtable) = std::mem::transmute_copy::<Dog, (*const u8, *const usize)>(&dog);
+                println!("animal_pointer: {:?}", animal_pointer);
+                println!("animal_vtable: {:?}", animal_vtable);
+                println!("cat_pointer: {:?}", cat_pointer);
+                println!("cat_vtable: {:?}", cat_vtable);
+                println!("dog_pointer: {:?}", dog_pointer);
+                println!("dog_vtable: {:?}", dog_vtable);
+                //println!("animal_ptr metadata: {:?}", core::intrinsics::ptr_metadata(animal_pointer));
+                //println!("animal_vtable ptr metadata: {:?}", core::intrinsics::ptr_metadata(animal_vtable));
+                //println!("+0: {:?}", *(animal_vtable));
+                //println!("+1: {:?}", *(animal_vtable.add(1)));
+                //println!("+2: {:?}", *(animal_vtable.add(2)));
+                //println!("+3: {:?}", *(animal_vtable.add(3)));
+                //println!("+4: {:?}", *(animal_vtable.add(4)));
+                //println!("+5: {:?}", *(animal_vtable.add(5)));
+                //println!("+6: {:?}", *(animal_vtable.add(6)));
+                //println!("+7: {:?}", *(animal_vtable.add(7)));
+            }
             let typeid = animal.type_id();
             let rawptr = Box::into_raw(animal) as *const ();
-            println!("dyn animal typeid: {:?}", typeid);
-            println!("cat typeid: {:?}", TypeId::of::<Cat>());
-            println!("dog typeid: {:?}", TypeId::of::<Dog>());
+            //println!("dyn animal typeid: {:?}", typeid);
+            //println!("cat typeid: {:?}", TypeId::of::<Cat>());
+            //println!("dog typeid: {:?}", TypeId::of::<Dog>());
 
             if typeid == TypeId::of::<Cat>() { //core::intrinsics::type_id::<Cat>() {
                 unsafe {

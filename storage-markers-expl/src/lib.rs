ef94e2d93489aec60b0180875d16982d85a1bac6
gakalaba <akalaba@princeton.edu>
2026-02-25 03:10:26 +0000

    added 2 programs based on test from compiler @ tests/ui/codegen/StackColoring-not-blowup-stack-issue-40883.rs; first one is copy second one is stack over flow

--- storage-markers-expl/src/lib.rs
+++ storage-markers-expl/src/lib.rs
@@ -2,3 +2,5 @@ pub mod recurse_large_array;
 pub mod heap_dep_stack;
 pub mod cereal;
 pub mod cereal_200;
+pub mod blowup_stack;
+pub mod stack_overflow;
---
+++ storage-markers-expl/src/stack_overflow.rs
@@ -0,0 +1,58 @@
+//@ run-pass
+//@ ignore-backends: gcc
+
+#![allow(dead_code)]
+// check that we don't have linear stack usage with multiple calls to `push`
+
+use std::mem;
+use std::hint::black_box;
+
+fn meal() -> Big {
+    if black_box(false) {
+        panic!()
+    }
+    Big { drop_me: [0; 2_000_000]}
+}
+
+pub struct Big {
+    drop_me: [u8; 2_000_000],
+}
+
+// THIS TEST WON'T WORK IF THIS INLINE IS MISSING!!!
+#[inline]
+fn push(out: &mut Vec<Big>) {
+    out.push(meal());
+}
+
+#[inline(never)]
+pub fn supersize_me(out: &mut Vec<Big>) {
+    push(out);
+    push(out);
+    push(out);
+    // Add another one for stack overflow when using no storage markers!
+    // push(out);
+}
+
+#[inline(never)]
+fn verify_stack_usage(before_ptr: *mut Vec<Big>) {
+    // To check stack usage, create locals before and after
+    // and check the difference in addresses between them.
+    let mut stack_var: Vec<Big> = vec![];
+    black_box(&mut stack_var);
+    let stack_usage = isize::abs(
+        (&mut stack_var as *mut _ as isize) -
+            (before_ptr as isize)) as usize;
+    // Give space for 2 copies of `Big` + 272 "misc" bytes
+    // (value observed on x86_64-pc-windows-gnu).
+    // if stack_usage > mem::size_of::<Big>() * 2 + 272 {
+    //     panic!("used {} bytes of stack, but `struct Big` is only {} bytes",
+    //            stack_usage, mem::size_of::<Big>());
+    // }
+    // println!("used {} bytes of stack, should at most be {}", stack_usage, mem::size_of::<Big>() * 2 + 272);
+}
+
+pub fn main() {
+    let mut v = vec![];
+    black_box(&mut v);
+    supersize_me(&mut v);
+}

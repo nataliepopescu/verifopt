bc3a80cab2612440c4920240df82293bd3e9b1be
Natalie Popescu <nataliepopescu@gmail.com>
2026-04-06 17:36:32 -0400

    add rand example

---
+++ examples/two_variants_rand/src/main.rs
@@ -0,0 +1,60 @@
+#![feature(ptr_metadata)]
+#![allow(dead_code)]
+
+extern crate rand;
+use rand::RngExt;
+
+pub trait Animal {
+    fn speak(&self) -> usize;
+    fn walk(&self) -> usize;
+}
+
+#[inline(never)]
+pub fn get_animal(num: usize) -> Box<dyn Animal> {
+    if num == 0 {
+        Box::new(Cat {})
+    } else {
+        Box::new(Dog {})
+    }
+}
+
+#[inline(always)]
+pub fn get_cat() -> Box<dyn Animal> {
+    return Box::new(Cat {});
+}
+
+#[inline(always)]
+pub fn get_dog() -> Box<dyn Animal> {
+    return Box::new(Dog {});
+}
+
+pub struct Cat;
+pub struct Dog;
+
+impl Animal for Cat {
+    fn speak(&self) -> usize {
+        11111
+    }
+    fn walk(&self) -> usize {
+        33333
+    }
+}
+
+impl Animal for Dog {
+    fn speak(&self) -> usize {
+        22222
+    }
+    fn walk(&self) -> usize {
+        44444
+    }
+}
+
+fn main() {
+    let x = rand::rng().random_range(..2usize); //args[1].parse().unwrap();
+    let animal = get_animal(x);
+    let cat = get_animal(0);
+    let _animal_vtable = core::ptr::metadata(&*animal);
+    let _cat_vtable = core::ptr::metadata(&*cat);
+    let res = animal.speak();
+    println!("res: {:?}", res);
+}

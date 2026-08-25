b2a8202394e60cd627febed533914865d5e765b1
Natalie Popescu <nataliepopescu@gmail.com>
2026-08-25 18:17:38 -0400

    use TyCtxt

--- safeose/src/safeose.rs
+++ safeose/src/safeose.rs
@@ -5,6 +5,7 @@ extern crate rustc_middle;
 use rustc_driver::{Callbacks, Compilation};
 use rustc_interface::interface::Compiler;
 use rustc_middle::ty::TyCtxt;
+use rustc_public::rustc_internal;
 
 use crate::start_safeose;
 use crate::util::options::AnalysisOptions;
@@ -14,8 +15,10 @@ pub struct SafeOSECallbacks {
 }
 
 impl Callbacks for SafeOSECallbacks {
-    fn after_analysis<'tcx>(&mut self, _compiler: &Compiler, _tcx: TyCtxt<'tcx>) -> Compilation {
-        start_safeose(self.options.clone());
+    fn after_analysis<'tcx>(&mut self, _compiler: &Compiler, tcx: TyCtxt<'tcx>) -> Compilation {
+        rustc_internal::run(tcx, || {
+            start_safeose(self.options.clone());
+        }).unwrap();
 
         Compilation::Continue
     }

6679414d858a7302ba25701a3d4cfab9a2a2251c
Natalie Popescu <nataliepopescu@gmail.com>
2026-08-31 15:57:05 -0400

    votrace interaction is exposing some weird bug in MIR->LLVM translation - commented out for now

--- testing_examples/two_variants/src/main.rs
+++ testing_examples/two_variants/src/main.rs
@@ -31,7 +31,7 @@ pub fn get_dog() -> Box<dyn Animal> {
 pub struct Cat;
 pub struct Dog;
 
-#[votrace::trace]
+//#[votrace::trace]
 impl Animal for Cat {
     fn speak(&self) -> usize {
         11111
@@ -41,7 +41,7 @@ impl Animal for Cat {
     }
 }
 
-#[votrace::trace]
+//#[votrace::trace]
 impl Animal for Dog {
     fn speak(&self) -> usize {
         22222

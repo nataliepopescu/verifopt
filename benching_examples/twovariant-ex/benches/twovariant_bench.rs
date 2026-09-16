9f0b6ae362c730535f4e881f61573dd315aa4deb
Natalie Popescu <nataliepopescu@gmail.com>
2026-09-16 17:51:25 -0400

    clean up twovariant example

--- benching_examples/twovariant-ex/benches/twovariant_bench.rs
+++ benching_examples/twovariant-ex/benches/twovariant_bench.rs
@@ -1,49 +1,33 @@
 use criterion::{BatchSize, Criterion, criterion_group, criterion_main};
 use twovariant_ex::{Animal, Cat, wrap_dyn_call_from_inner, wrap_dyn_call_from_outer, wrap_cat_call};
 
-pub fn run_inner_setup(a: &dyn Animal) -> usize {
-    wrap_dyn_call_from_inner(a)
-}
-
-pub fn run_outer_setup(a: &dyn Animal) -> usize {
-    wrap_dyn_call_from_outer(a)
-}
-
-//pub fn run_no_setup(a: &dyn Animal) -> usize {
-//    wrap_dyn_call(a)
-//}
-
-pub fn run_cat(c: &Cat) -> usize {
-    wrap_cat_call(c)
-}
-
 fn bench_twovariant(c: &mut Criterion) {
     let mut group = c.benchmark_group("twovariant");
     group.bench_function("twovariant_inner", |b| {
         b.iter_batched(
             || Box::new(Cat {}),
-            |animal| std::hint::black_box(run_inner_setup(&*animal)),
+            |animal| std::hint::black_box(wrap_dyn_call_from_inner(&*animal)),
             BatchSize::SmallInput,
         )
     });
     group.bench_function("twovariant_outer", |b| {
         b.iter_batched(
             || Box::new(Cat {}),
-            |animal| std::hint::black_box(run_outer_setup(&*animal)),
+            |animal| std::hint::black_box(wrap_dyn_call_from_outer(&*animal)),
             BatchSize::SmallInput,
         )
     });
     //group.bench_function("twovariant_dynamic", |b| {
     //    b.iter_batched(
     //        || get_animal(x),
-    //        |animal| std::hint::black_box(run_no_setup(&*animal)),
+    //        |animal| std::hint::black_box(wrap_dyn_call(&*animal)),
     //        BatchSize::SmallInput,
     //    )
     //});
     group.bench_function("twovariant_static", |b| {
         b.iter_batched(
             || Cat {},
-            |cat| std::hint::black_box(run_cat(&cat)),
+            |cat| std::hint::black_box(wrap_cat_call(&cat)),
             BatchSize::SmallInput,
         )
     });

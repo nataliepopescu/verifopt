7e36dad9524a0e353b8bf72ec052592560439e9b
Natalie Popescu <nataliepopescu@gmail.com>
2026-09-16 17:51:37 -0400

    add caller/genargs to stats file

--- monomorph/src/logger.rs
+++ monomorph/src/logger.rs
@@ -32,9 +32,9 @@ impl VOLogger {
             let cha = dispatch_cha.get(key).cloned().unwrap().1;
 
             if cha.len() == fsa.len() {
-                same.push((*span, cha, fsa.clone()));
+                same.push((key.clone(), *span, cha, fsa.clone()));
             } else {
-                diff.push((*span, cha, fsa.clone()));
+                diff.push((key.clone(), *span, cha, fsa.clone()));
             }
         }
 
@@ -46,10 +46,14 @@ impl VOLogger {
         )?;
 
         write!(&mut self.stats_file, "--MAYBE EXAMPLES--\n")?;
-        for (span, cha, fsa) in &diff {
+        for (key, span, cha, fsa) in &diff {
+            let (caller_def_id, bb_index, caller_genargs) = key;
             write!(
                 &mut self.stats_file,
-                "Span: {:?}\nCHA ({}): {:?}\nFSA ({}): {:?}\n\n",
+                "Caller: {:?} (bb {}), caller genargs: {:?}\nSpan: {:?}\nCHA ({}): {:?}\nFSA ({}): {:?}\n\n",
+                caller_def_id,
+                bb_index,
+                caller_genargs,
                 span,
                 cha.len(),
                 cha,
@@ -59,10 +63,14 @@ impl VOLogger {
         }
 
         write!(&mut self.stats_file, "--NOT EXAMPLES--\n")?;
-        for (span, cha, fsa) in &same {
+        for (key, span, cha, fsa) in &same {
+            let (caller_def_id, bb_index, caller_genargs) = key;
             write!(
                 &mut self.stats_file,
-                "Span: {:?}\nCHA ({}): {:?}\nFSA ({}): {:?}\n",
+                "Caller: {:?} (bb {}), caller genargs: {:?}\nSpan: {:?}\nCHA ({}): {:?}\nFSA ({}): {:?}\n",
+                caller_def_id,
+                bb_index,
+                caller_genargs,
                 span,
                 cha.len(),
                 cha,

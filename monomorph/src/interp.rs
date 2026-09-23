3bd79db60671d5e64f61e0b49cdc86c98e54b8e5
Natalie Popescu <nataliepopescu@gmail.com>
2026-09-23 12:15:11 -0400

    actually ignore inlineasm this time; comment out noisy logging

--- monomorph/src/interp.rs
+++ monomorph/src/interp.rs
@@ -353,7 +353,7 @@ impl<'a> InterpPass<'a> {
             *self.bb_visit_count.borrow(),
             self.run_start.elapsed().as_secs_f64() * 1000.0
         );
-        crate::constraints::dump_flatten_all_cache_stats("FINAL");
+        //crate::constraints::dump_flatten_all_cache_stats("FINAL");
         self.dump_self_time_report("FINAL", &self.scope_self_time_global.borrow());
         self.dump_timing_report("FINAL GLOBAL", &self.timing_global.borrow());
         self.dump_timing_by_scope_report("FINAL", &self.timing_scope_global.borrow());
@@ -498,7 +498,7 @@ impl<'a> InterpPass<'a> {
                         *n,
                         self.run_start.elapsed().as_secs_f64() * 1000.0
                     );
-                    crate::constraints::dump_flatten_all_cache_stats(&format!("bb visit {}", *n));
+                    //crate::constraints::dump_flatten_all_cache_stats(&format!("bb visit {}", *n));
                     //self.log_bb_cache_sizes(*n, cur_scope, ctxt, bb_deps.ordering.len());
                     self.dump_self_time_report(
                         &format!("bb visit {}", *n),
@@ -1179,23 +1179,9 @@ impl<'a> InterpPass<'a> {
                 r
             }
             TerminatorKind::Assert { .. }
+            | TerminatorKind::InlineAsm { .. }
             | TerminatorKind::Drop { .. }
             | TerminatorKind::Goto { .. } => Ok(None),
-            /*
-            _iasm @ TerminatorKind::InlineAsm {
-                ..
-                //template,
-                //operands,
-                //options,
-                //line_spans,
-                //destination,
-                //unwind
-            } => {
-                //debug!("iasm: {:#?}", iasm);
-                // TODO do not interp, try to get rettype
-                todo!("inline asm");
-            }
-            */
             _ => todo!("other term kind: {:?}", &term.kind),
         }
     }
